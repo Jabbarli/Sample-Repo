e9651a1cd73ab58754f618a6211ccd8478729311
Azar Jabbarli <jabbarli.azar@gmail.com>
2021-01-23 10:38:55 -0500

    Removed some code

--- script.py
+++ script.py
@@ -1,14 +1,5 @@
-import sys
 import requests
-
-print(sys.executable)
-print(sys.version)
-
-
-def greet(who_to_greet):
-    greeting = f"Hello, {who_to_greet}"
-    return greeting
-
 
 r = requests.get("https://www.google.ca/")
 print(r.status_code)
+print(r.ok)
